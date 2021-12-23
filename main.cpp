419d3a5ac451edbdf49979689098c1194e2483ea
ArcEstBow <92146311+ArcEstEnTrainDeCoder@users.noreply.github.com>
2021-12-23 16:45:12 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include <thread>
 #include <chrono>
 #include<unistd.h>
-#include <ctime>
 
-#include "mingl/gui/text.h"
+//CHANGER LES CHEMINS !!!
 
-#include "mingl/shape/line.h"
+#include "MinGL/include/mingl/gui/text.h"
 
-#include "mingl/shape/rectangle.h"
-#include "mingl/mingl.h"
+#include "MinGL/include/mingl/shape/line.h"
 
-#include "mingl/gui/sprite.h"
+#include "MinGL/include/mingl/shape/rectangle.h"
 
-#include "mingl/graphics/vec2d.h"
-#include "mingl/shape/rectangle.h"
+#include "MinGL/include/mingl/mingl.h"
 
+#include "MinGL/include/mingl/gui/sprite.h"
+
+#include <MinGL/include/mingl/graphics/vec2d.h>
+
+#include "MinGL/include/mingl/shape/rectangle.h"
+
+//CHANGER LES CHEMINS !!
 
 using namespace std;
 using namespace nsGraphics;
@@ -63,13 +67,24 @@ void keyboard(MinGL &window, Sprite &mug)
         mug.setPosition(positionF);
     }
 }
+//Verify if an ennmy is still alive
+bool allDead(const enemy & PPs) {
+    for (size_t i = 0; i < PPs.state.size(); ++i) {
+        //If the ennemy is alive
+        if (PPs.state[i] == true) {
+            return false;
+        }
+    }
+    //There is no enemy alive anymore
+    return true;
+}
 
 bool isTouching (const Vec2D firstCorner, const Vec2D secondCorner, const Vec2D test){
     return ((test.getX() <= secondCorner.getX() && test.getY() <= secondCorner.getY()) && (test.getX() >= firstCorner.getX() && test.getY() >= firstCorner.getY()));
 }
 
 bool colision(const Vec2D misPos, enemy &vecSprite){
-    for (unsigned i = 0; i < vecSprite.vecSprite.size(); ++i) {
+    for (size_t i = 0; i < vecSprite.vecSprite.size(); ++i) {
         Vec2D a = vecSprite.vecSprite[i].getPosition();
         int bX = vecSprite.vecSprite[i].getPosition().getX()+55;
         int bY = vecSprite.vecSprite[i].getPosition().getY()+50;
@@ -155,6 +170,28 @@ void move(Sprite &position, const int &x, const int &y) {
     position.setPosition(Vec2D(position.getPosition().getX() + x, position.getPosition().getY() + y));
 }
 
+void moveOpen(enemy & open) {
+    if (open.vecSprite[0].getPosition().getX() < (600 - 470) || open.vecSprite[open.vecSprite.size() - 1].getPosition().getX() > 50) {
+        for(Sprite & letter : open.vecSprite) {
+            move(letter, open.rightOrLeft*5, 0);
+        }
+    }
+    //Marche
+    if (open.vecSprite[open.vecSprite.size() - 1].getPosition().getX() > (600 - 99) && open.rightOrLeft == 1) {
+        open.rightOrLeft = -1;
+        for (Sprite & letter : open.vecSprite) {
+            move(letter, 0, 10);
+        }
+    }
+    else if(open.vecSprite[0].getPosition().getX() <  (600 - 470) && open.rightOrLeft == -1) {
+        open.rightOrLeft = 1;
+        for (Sprite & letter : open.vecSprite) {
+            move(letter, 0, 10);
+        }
+    }
+    if (open.vecSprite[0].getPosition().getY() > 600) exit(0);
+}
+
 void moveVecSprite(enemy &vecSprite){
     // Si les sprites au extrémité ne touches pas les bords, bouger tout les sprites en même temps
     if (vecSprite.vecSprite[0].getPosition().getX() < (600-64+50) ||
@@ -180,7 +217,7 @@ void moveVecSprite(enemy &vecSprite){
 
 void generateVecSprite(enemy &IPPs, const int posY, const string pathSprite){
     // liste de sprite
-    for (unsigned i = 0; i < 5; ++i) {
+    for (size_t i = 0; i < 5; ++i) {
         Vec2D ipp;
         ipp.setX(50+124+72*i);
         ipp.setY(138+posY);
@@ -188,6 +225,108 @@ void generateVecSprite(enemy &IPPs, const int posY, const string pathSprite){
         IPPs.vecSprite.push_back(sprite);
         IPPs.state.push_back(true);
     }
+}
+
+void generateOPEN(enemy & open, const int posY) {
+    //Generate letters one by one
+    Vec2D firstO;
+    firstO.setX(280);
+    firstO.setY(138+posY);
+
+    Vec2D p;
+    p.setX(firstO.getX()+33);
+    p.setY(138+posY);
+
+    Vec2D e;
+    e.setX(p.getX()+33);
+    e.setY(138+posY);
+
+    Vec2D n;
+    n.setX(e.getX()+33);
+    n.setY(138 + posY);
+
+    Sprite spriteO("spritesi2/first-O.si2", firstO);
+    Sprite spriteP("spritesi2/P.si2", p);
+    Sprite spriteE("spritesi2/E.si2", e);
+    Sprite spriteN("spritesi2/N.si2", n);
+
+    //Gathers very sprite in a vector so we can add them to OPEN
+    vector<Sprite> vecOPEN = {spriteO, spriteP, spriteP, spriteE, spriteN};
+    for (size_t i = 0; i < vecOPEN.size(); ++i) {
+        open.vecSprite.push_back(vecOPEN[i]);
+        open.state.push_back(true);
+    }
+}
+
+void generateCLASSROOM(enemy & classroom, const int posY) {
+    //Generate all the letters one by one
+
+    //Genretate C
+    Vec2D c;
+    c.setX(200);
+    c.setY(105+posY);
+
+    //Generate L
+    Vec2D l;
+    l.setX(c.getX()+33);
+    //We use the .getX() of the previous letter
+    l.setY(105+posY);
+
+    //Generate A
+    Vec2D a;
+    a.setX(l.getX()+33);
+    a.setY(105+posY);
+
+    //Generate S
+    Vec2D s;
+    s.setX(a.getX()+33);
+    s.setY(105+posY);
+
+    //Generate the second S
+    Vec2D sTwo;
+    sTwo.setX(s.getX()+33);
+    sTwo.setY(105+posY);
+
+    //Generate R
+    Vec2D r;
+    r.setX(sTwo.getX()+33);
+    r.setY(105+posY);
+
+    //Generate O
+    Vec2D o;
+    o.setX(r.getX()+33);
+    o.setY(105+posY);
+
+    //Generate the second O
+    Vec2D oTwo;
+    oTwo.setX(o.getX()+33);
+    oTwo.setY(105+posY);
+
+    //Generate M
+    Vec2D m;
+    m.setX(oTwo.getX()+33);
+    m.setY(105+posY);
+
+    //Create sprites with the Vec2D
+    Sprite spriteC("spritesi2/C.si2", c);
+    Sprite spriteL("spritesi2/L.si2", l);
+    Sprite spriteA("spritesi2/A.si2", a);
+    Sprite spriteS("spritesi2/S.si2", s);
+    Sprite spriteSTwo("spritesi2/S.si2", sTwo);
+    Sprite spriteR("spritesi2/R.si2", r);
+    Sprite spriteO("spritesi2/O.si2", o);
+    Sprite spriteOTwo("spritesi2/O.si2", oTwo);
+    Sprite spriteM("spritesi2/M.si2", m);
+
+    //Gather all the sprites in a vector
+    vector<Sprite> vecClassroom = {spriteC, spriteL, spriteA, spriteS, spriteSTwo, spriteR, spriteO, spriteOTwo, spriteM};
+
+    //Adding all the sprite in the CLASSROOM vecSprite
+    for (size_t i = 0; i < vecClassroom.size(); ++i) {
+        classroom.vecSprite.push_back(vecClassroom[i]);
+        classroom.state.push_back(true);
+    }
+
 }
 
 void generateVecMug(mugStruct &mug){
@@ -212,17 +351,25 @@ int main()
 
     Sprite back("spritesi2/back.si2", Vec2D(0, 0));
 
+    enemy open;
+    open.rightOrLeft = 1;
+    generateOPEN(open, 50);
+
+    enemy classroom;
+    classroom.rightOrLeft = 1;
+    generateCLASSROOM(classroom, 125);
+
     enemy IPPs;
     IPPs.rightOrLeft = 1;
     generateVecSprite(IPPs, 50, "spritesi2/i++.si2");
 
-    enemy KPPs;
-    KPPs.rightOrLeft = 1;
-    generateVecSprite(KPPs, 100, "spritesi2/k++.si2");
-
     enemy JPPs;
     JPPs.rightOrLeft = 1;
-    generateVecSprite(JPPs, 150, "spritesi2/j++.si2");
+    generateVecSprite(JPPs, 100, "spritesi2/j++.si2");
+
+    enemy KPPs;
+    KPPs.rightOrLeft = 1;
+    generateVecSprite(KPPs, 150, "spritesi2/k++.si2");
 
     mugStruct mug;
     generateVecMug(mug);
@@ -269,12 +416,13 @@ int main()
         isPressed = missile(window, mug.vecMug[mug.index], IPPs, KPPs, JPPs, playerLifeUnsigned, firstShootM, isPressed);
         if(isPressed == true) window << nsShape::Rectangle(misPos, misPos + Vec2D(2, 10), KCyan);
         string playerLifeString = to_string(playerLifeUnsigned);
+        //Points generator
         window << Text(nsGraphics::Vec2D(60, 160), "Pts:", nsGraphics::KWhite, nsGui::GlutFont::BITMAP_9_BY_15);
         window << Text(nsGraphics::Vec2D(100, 160), playerLifeString, nsGraphics::KWhite, nsGui::GlutFont::BITMAP_9_BY_15);
 
         if (torpedo(mug, IPPs, firstShootT)) window << nsShape::Rectangle(torPos, torPos + Vec2D(5, 10), KGreen);
 
-        if (playerLifeUnsigned == 15) win(window);
+        //if (playerLifeUnsigned == 15) win(window);
 
 
         // On finit la frame en cours
@@ -288,6 +436,53 @@ int main()
 
         // On récupère le temps de frame
         frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
+
+        //Verify if enemies are still alive
+        if (allDead(IPPs) && allDead(JPPs) && allDead(KPPs)) {
+            while (!allDead(open)) {
+                // Récupère l'heure actuelle
+                chrono::time_point<chrono::steady_clock> start = chrono::steady_clock::now();
+
+                window.clearScreen();
+
+                //On fait tourner les procédures
+
+                window << back;
+                window << mug.vecMug[mug.index];
+
+                keyboard(window, mug.vecMug[mug.index]);
+
+
+                open.update(window);
+                classroom.update(window);
+
+                //OPEN and CLASSROOM movements
+
+                moveVecSprite(classroom);
+                moveOpen(open);
+
+                isPressed = missile(window, mug.vecMug[mug.index], IPPs, KPPs, JPPs, playerLifeUnsigned, firstShootM, isPressed);
+                if(isPressed == true) window << nsShape::Rectangle(misPos, misPos + Vec2D(2, 10), KCyan);
+                string playerLifeString = to_string(playerLifeUnsigned);
+                //Points generator
+                window << Text(nsGraphics::Vec2D(60, 160), "Pts:", nsGraphics::KWhite, nsGui::GlutFont::BITMAP_9_BY_15);
+                window << Text(nsGraphics::Vec2D(100, 160), playerLifeString, nsGraphics::KWhite, nsGui::GlutFont::BITMAP_9_BY_15);
+
+                if (torpedo(mug, open, firstShootT)) window << nsShape::Rectangle(torPos, torPos + Vec2D(5, 10), KGreen);
+
+
+                window.finishFrame();
+
+                window.getEventManager().clearEvents();
+
+                // On attend un peu pour limiter le framerate et soulager le CPU
+                this_thread::sleep_for(chrono::milliseconds(3000 / FPS_LIMIT) - chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start));
+
+                // On récupère le temps de frame
+                frameTime = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start);
+
+            }
+        }
     }
     return 0;
 }
