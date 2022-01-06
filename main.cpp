6014e6c71671d27c6e7866b8dc3ce093a2623340
Calliouxes <92147165+EscalierE@users.noreply.github.com>
2022-01-06 12:45:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 #include "menu.h"
 #include "mystruct.h"
 #include "move.h"
+#include "check.h"
 #include "generate.h"
 
 using namespace std;
@@ -137,20 +138,20 @@ int main()
         if (choixobjet==0) { //Casali shooter home screen opening
             window.clearScreen();
             choixLightDark(window,choixpsgom, background, backgroundpsg);
-            dessiner(window, titreaccueil);
-            dessiner(window, startb);
-            dessiner(window, scoreb);
-            dessiner(window, settingsb);
-            dessiner(window, quitb);
-            dessiner(window, casali);
+            window << window, titreaccueil;
+            window << window, startb;
+            window << window, scoreb;
+            window << window, settingsb;
+            window << window, quitb;
+            window << window, casali;
             menu(window, casali);
             choixobjet = entrerMenu(window, casali);
         }else if (choixobjet == 2){ //Opening of the scoreboard menu
             window.clearScreen();
             choixLightDark(window,choixpsgom, background, backgroundpsg);
-            dessiner(window, scoreboardbg);
-            dessiner(window, titrescoreboard);
-            dessiner(window, backb);
+            window << window, scoreboardbg;
+            window << window, titrescoreboard;
+            window << window, backb;
             if (window.isPressed({'b', false})) {
                 choixobjet = 0;
             }
@@ -159,10 +160,10 @@ int main()
             window.clearScreen();
             choixLightDark(window,choixpsgom, background, backgroundpsg);
             window << choosethemode;
-            dessiner(window, moon);
-            dessiner(window, sun);
-            dessiner(window, arrow);
-            dessiner(window, backb);
+            window << window, moon;
+            window << window, sun;
+            window << window, arrow;
+            window << window, backb;
             selectTheme(window, arrow);
             choixpsgom = chooseTheme(window, arrow, choixpsgom);
             if (window.isPressed({'b', false})) {
